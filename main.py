a1c1d0d53cea462dfb86b5da5223a60d33dd08b7
Mixail132 <millionbaxov@gmail.com>
2025-09-07 15:07:08 +0300

    Add simple google generative AI ligic.

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from model import model
 
 app = FastAPI()
 
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+
+def gemini_ai(prompt):
+    prompt = prompt
+    response = model.generate_content(contents=[prompt])
+    return response.text
 
 
 app.add_middleware(
@@ -23,5 +30,6 @@ async def serve_home():
 @app.post("/chat")
 async def chat(request: Request):
     data = await request.json()
-    user_message = data.get("message", "")
-    return JSONResponse(content={"response": f"You said: {user_message}"})
+    prompt = data.get("message", "")
+    response = model.generate_content(contents=[prompt])
+    return JSONResponse(content={"response": f"Gemini: {response}"})
